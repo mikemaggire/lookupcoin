fee03505367d2b8f00c755bdd796ffc1a1ffaa84
mikemaggire <mikemaggire@gmail.com>
2022-02-21 18:48:45 +0100

    cleaning comments

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+Copyright © 2022 Mike Maggire <mike.maggire@gmail.com>
 
 */
 package cmd
@@ -27,8 +27,6 @@ var rootCmd = &cobra.Command{
 	Returns one line per finding.
 
 PARAMETER: the coin your're looking for.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) { 
 		if len(args) == 0 || len(args[0]) == 0 {
 			fmt.Println("Missing search parameter")
@@ -64,43 +62,39 @@ func Execute() {
 
 func init() {
 	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.lookupcoin.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("exact-match", "", false, "TRUE to lookup the exact string, FALSE to lookup the string everywhere in fields.")
 }
 
+// return a slice of founded coins, with formatted strings
 func lookupCoin(search string, fExactMatch bool) ([]string, error) {
 
+	// load coinGecko database
 	httpClient := &http.Client{	Timeout: time.Second * 15 }
 	cg := coingecko.NewClient(httpClient)
 	gclist, err := cg.CoinsList()
 	if err != nil {
 		return nil, err
 	}
-
 	fmt.Printf("Lookup among %d coins in the CoinGecko database.\n", len(*gclist))
 
+	// prepare for the lookup
 	s := make([]string, 0)
 	lsearch := strings.ToLower(search)
 
-	// lookup to find the symbol
+	// lookup
 	for _, gc := range *gclist {
 		fMatch := false
 		if fExactMatch {
 			if 	(strings.ToLower(gc.Symbol) == lsearch) || (strings.ToLower(gc.ID) == lsearch) {
 				fMatch = true
 			}
-		} else {
+		} else { // !fExactMatch
 			gcdata := gc.Symbol + "•" + gc.ID + "•" + gc.Name
 			if strings.Contains(strings.ToLower(gcdata), lsearch) {
 				fMatch = true
 			}
 		}
+		// Format the found coin
 		if fMatch {
 			found := util.White(util.Tab(gc.Symbol, 15, false) + ": ")
 			found += gc.ID + ", "
@@ -108,6 +102,8 @@ func lookupCoin(search string, fExactMatch bool) ([]string, error) {
 			s = append(s, found)
 		}
 	}
+
+	// That's it
 	if len(s) == 0 {
 		return nil, errors.New("no coin matches '"+ search + "'")
 	}
